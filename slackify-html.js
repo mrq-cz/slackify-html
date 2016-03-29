4da3d36c06d44a4eab09eff1c4d0c26557385388
Pete Michel <peterman225@gmail.com>
2016-03-29 11:33:02 -0400

    adding support for lists, headers, pre/code, and tables

--- slackify-html.js
+++ slackify-html.js
@@ -14,7 +14,121 @@ module.exports = function slackify(html) {
     return entities.decode(walk(dom));
   else
     return '';
+};
+
+function walkList(dom, ordered) {
+  var out = '';
+  if (dom) {
+    var listItemIndex = 1;
+    dom.forEach(function (el) {
+      if ('text' === el.type) {
+        if (el.data.trim() !== '') {
+          out += el.data;
+        }
+      }
+      else if ('tag' === el.type) {
+        switch (el.name) {
+          case 'li':
+            out += (ordered ? listItemIndex++ + '. ' : "* ") + walk(el.children) + '\n';
+            break;
+          default:
+            out += walk(el.children);
+        }
+      }
+    });
+  }
+  return out;
 }
+
+function walkPre(dom) {
+  var out = '';
+  if (dom) {
+    dom.forEach(function (el) {
+      if ('text' === el.type) {
+        out += el.data;
+      }
+      else if ('tag' === el.type) {
+        out += walkPre(el.children);
+      }
+    });
+  }
+  return out;
+}
+
+function walkTable(dom) {
+  var out = '';
+  if (dom) {
+    dom.forEach(function (el) {
+      if ('tag' === el.type) {
+        if ('thead' === el.name) {
+          out += walkTableHead(el.children);
+        }
+        else if ('tbody' === el.name) {
+          out += walkTableBody(el.children);
+        }
+      }
+    });
+  }
+
+  return out;
+}
+
+function walkTableHead(dom) {
+  var out = '';
+  if (dom) {
+    var headers = [];
+    dom.forEach(function (el) {
+      if ('text' === el.type) {
+        if (el.data.trim() !== '') {
+          out += el.data;
+        }
+      }
+      else if ('tr' === el.name) {
+        out += walkTableHead(el.children);
+      }
+      else if ('th' === el.name) {
+        var header = walkTableHead(el.children);
+        headers.push(header);
+        out += '| ' + header + ' ';
+      }
+    });
+    if (headers.length > 0) {
+      out += ' |\n';
+      headers.forEach(function (item) {
+        out += '| ';
+        for (i = 0; i < item.length; i++) {
+          out += '-';
+        }
+        out += ' ';
+      });
+      out += ' |\n';
+    }
+  }
+
+  return out;
+}
+
+function walkTableBody(dom) {
+  var out = '';
+  if (dom) {
+    dom.forEach(function (el) {
+      if ('text' === el.type) {
+        if (el.data.trim() !== '') {
+          console.log('###' + el.data + '###');
+          out += el.data;
+        }
+      }
+      else if ('td' === el.name) {
+        out += '| ' + walkTableBody(el.children) + ' ';
+      }
+      else if ('tr' === el.name) {
+        out += walkTableBody(el.children) + '|\n';
+      }
+    });
+  }
+  return out;
+}
+
 
 function walk(dom) {
   var out = '';
@@ -23,7 +137,7 @@ function walk(dom) {
       if ('text' === el.type) {
         out += el.data;
       }
-      if ('tag' === el.type) {
+      else if ('tag' === el.type) {
         switch (el.name) {
           case 'a':
             out += '<' + el.attribs.href + '|' + walk(el.children) + '>';
@@ -36,6 +150,32 @@ function walk(dom) {
           case 'em':
             out += '_' + walk(el.children) + '_';
             break;
+          case 'p':
+            out += walk(el.children) + '\n';
+            break;
+          case 'br':
+            out += walk(el.children) + '\n';
+            break;
+          case 'h1':
+          case 'h2':
+          case 'h3':
+          case 'h4':
+            out += '*' + walk(el.children) + '*';
+            break;
+          case 'ol':
+          case 'ul':
+            out += walkList(el.children, 'ol' === el.name);
+            break;
+          case 'code':
+            out += '`' + walk(el.children) + '`';
+            break;
+          case 'pre':
+            out += '```\n' + walkPre(el.children) + '```';
+            break;
+          case 'table':
+            out += walkTable(el.children);
+            break;
+          //FIXME IMG
           default:
             out += walk(el.children);
         }
--- tests.js
+++ tests.js
@@ -30,3 +30,54 @@ tap.test('vcheck example', function vcheck(t) {
     '*2.4-SNAPSHOT* • revision <https://a-team.csint.cz/stash/projects/webapi/repos/webapi/commits/a245dc97ec7bbe9ed6cb9fc1025dd846fefc7c89|a245dc9> • build 2015-09-07 14:06 • wbl 1.3.33 • <http://www.csast.csas.cz/at/webapi/api/v1/version|details »>');
   t.end();
 });
+
+tap.test('full example', function vcheck(t) {
+  var input = `<div class="ghq-markdown-content" ><h2>Security Overview Header</h2>
+<p><strong>We take the security of your data very seriously!</strong></p>
+<p>In order to instill the necessary confidence, we wanted to provide full transparency on <em>why</em>, <i>who</i>, where, when and how we protect your data.</p>
+<p>Given the sensitive nature of your content and need to maintain your privacy being a priority for us, we wanted to share the practices and policies we have put into place.</p>
+<p><a href="https://www.getguru.com/privacy/" target="_blank">Privacy Policy</a></p>
+<p>Remember this list</p>
+<ol>
+<li>foo</li>
+<li>bar</li>
+<li>buz</li>
+</ol>
+<p>and this list too...</p>
+<ul>
+<li>abc</li>
+<li>def</li>
+<li>xyz</li>
+</ul>
+<p><code>and this</code></p>
+<pre><code><span class="ghq-hljs-keyword">blah
+</span></code></pre>
+<p><img src="https://qaup.getguru.com/5240119b-9752-443a-9172-73204f8599eb/94acdc58-32c2-44d3-9843-7a2a5cb3fbf5.bc70afa3-1798-4c87-a2fe-21e3f855e35a.jpeg" alt=""></p>
+<table>
+<thead>
+<tr>
+<th>Column 1</th>
+<th>Column 2</th>
+<th>Column 3</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>Foo</td>
+<td>Bar</td>
+<td>Baz</td>
+</tr>
+<tr>
+<td>abc</td>
+<td>def</td>
+<td>ghi</td>
+</tr>
+</tbody>
+</table>
+</div>`;
+var expected = '*Security Overview Header*\n*We take the security of your data very seriously!*\n\n In order to instill the necessary confidence, we wanted to provide full transparency on _why_, _who_, where, when and how we protect your data.\n\n Given the sensitive nature of your content and need to maintain your privacy being a priority for us, we wanted to share the practices and policies we have put into place.\n\n<https://www.getguru.com/privacy/|Privacy Policy>\n\nRemember this list\n1. foo\n2. bar\n3. buz\n\nand this list too...\n* abc\n* def\n* xyz\n\n\`and this\`\n\n\`\`\`\nblah\n\`\`\`\n\n![](https://qaup.getguru.com/5240119b-9752-443a-9172-73204f8599eb/94acdc58-32c2-44d3-9843-7a2a5cb3fbf5.bc70afa3-1798-4c87-a2fe-21e3f855e35a.jpeg)\n\n\n| Column 1 | Column 2 | Column 3 |\n| -------- | -------- | -------- |\n| Foo | Bar | Baz |\n| abc | def | ghi |';
+var output = slackify(input);
+  t.equals(output,
+    expected);
+  t.end();
+});
